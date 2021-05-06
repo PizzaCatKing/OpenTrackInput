7ebf77a99c4539bfaff7b61231f05fb49020985c
Chris Dufour <chris@cdufour.com>
2021-05-05 23:10:58 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,251 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PortTB = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RawDataRollTB = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.RawDataPitchTB = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.RawDataYawTB = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.RawDataZTB = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.RawDataYTB = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.RawDataXTB = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.StopButton = New System.Windows.Forms.Button()
+        Me.LogRTB = New System.Windows.Forms.RichTextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Port"
+        '
+        'PortTB
+        '
+        Me.PortTB.Location = New System.Drawing.Point(48, 10)
+        Me.PortTB.Name = "PortTB"
+        Me.PortTB.Size = New System.Drawing.Size(100, 23)
+        Me.PortTB.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RawDataRollTB)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.RawDataPitchTB)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.RawDataYawTB)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.RawDataZTB)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.RawDataYTB)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.RawDataXTB)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 39)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(302, 115)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Raw Data"
+        '
+        'RawDataRollTB
+        '
+        Me.RawDataRollTB.Enabled = False
+        Me.RawDataRollTB.Location = New System.Drawing.Point(182, 74)
+        Me.RawDataRollTB.Name = "RawDataRollTB"
+        Me.RawDataRollTB.Size = New System.Drawing.Size(100, 23)
+        Me.RawDataRollTB.TabIndex = 15
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(147, 77)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(27, 15)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Roll"
+        '
+        'RawDataPitchTB
+        '
+        Me.RawDataPitchTB.Enabled = False
+        Me.RawDataPitchTB.Location = New System.Drawing.Point(182, 45)
+        Me.RawDataPitchTB.Name = "RawDataPitchTB"
+        Me.RawDataPitchTB.Size = New System.Drawing.Size(100, 23)
+        Me.RawDataPitchTB.TabIndex = 13
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(147, 48)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(34, 15)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Pitch"
+        '
+        'RawDataYawTB
+        '
+        Me.RawDataYawTB.Enabled = False
+        Me.RawDataYawTB.Location = New System.Drawing.Point(182, 16)
+        Me.RawDataYawTB.Name = "RawDataYawTB"
+        Me.RawDataYawTB.Size = New System.Drawing.Size(100, 23)
+        Me.RawDataYawTB.TabIndex = 11
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(147, 19)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(28, 15)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Yaw"
+        '
+        'RawDataZTB
+        '
+        Me.RawDataZTB.Enabled = False
+        Me.RawDataZTB.Location = New System.Drawing.Point(41, 74)
+        Me.RawDataZTB.Name = "RawDataZTB"
+        Me.RawDataZTB.Size = New System.Drawing.Size(100, 23)
+        Me.RawDataZTB.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 77)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(14, 15)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Z"
+        '
+        'RawDataYTB
+        '
+        Me.RawDataYTB.Enabled = False
+        Me.RawDataYTB.Location = New System.Drawing.Point(41, 45)
+        Me.RawDataYTB.Name = "RawDataYTB"
+        Me.RawDataYTB.Size = New System.Drawing.Size(100, 23)
+        Me.RawDataYTB.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 48)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(14, 15)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Y"
+        '
+        'RawDataXTB
+        '
+        Me.RawDataXTB.Enabled = False
+        Me.RawDataXTB.Location = New System.Drawing.Point(41, 16)
+        Me.RawDataXTB.Name = "RawDataXTB"
+        Me.RawDataXTB.Size = New System.Drawing.Size(100, 23)
+        Me.RawDataXTB.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(14, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "X"
+        '
+        'StartButton
+        '
+        Me.StartButton.Location = New System.Drawing.Point(154, 9)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(75, 23)
+        Me.StartButton.TabIndex = 5
+        Me.StartButton.Text = "Start"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'StopButton
+        '
+        Me.StopButton.Enabled = False
+        Me.StopButton.Location = New System.Drawing.Point(235, 9)
+        Me.StopButton.Name = "StopButton"
+        Me.StopButton.Size = New System.Drawing.Size(75, 23)
+        Me.StopButton.TabIndex = 6
+        Me.StopButton.Text = "Stop"
+        Me.StopButton.UseVisualStyleBackColor = True
+        '
+        'LogRTB
+        '
+        Me.LogRTB.Location = New System.Drawing.Point(12, 160)
+        Me.LogRTB.Name = "LogRTB"
+        Me.LogRTB.ReadOnly = True
+        Me.LogRTB.Size = New System.Drawing.Size(303, 140)
+        Me.LogRTB.TabIndex = 7
+        Me.LogRTB.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(332, 312)
+        Me.Controls.Add(Me.LogRTB)
+        Me.Controls.Add(Me.StopButton)
+        Me.Controls.Add(Me.StartButton)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.PortTB)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Open Track Input"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PortTB As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RawDataRollTB As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents RawDataPitchTB As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents RawDataYawTB As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents RawDataZTB As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents RawDataYTB As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents RawDataXTB As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents StartButton As Button
+    Friend WithEvents StopButton As Button
+    Friend WithEvents LogRTB As RichTextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,100 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Threading
+Imports Microsoft.VisualStudio.Threading
+
+Public Class Form1
+    Dim listening As Boolean = False
+
+    Dim cts As CancellationTokenSource
+
+    Private Sub log(text As String)
+        Me.Invoke(Sub() LogRTB.Text += $"{vbCrLf}{text}")
+    End Sub
+
+    Private Sub setValues(x As Double, y As Double, z As Double, yaw As Double, pitch As Double, roll As Double)
+        RawDataXTB.Text = x.ToString("N03")
+        RawDataYTB.Text = y.ToString("N03")
+        RawDataZTB.Text = z.ToString("N03")
+
+        RawDataPitchTB.Text = pitch.ToString("N03")
+        RawDataYawTB.Text = yaw.ToString("N03")
+        RawDataRollTB.Text = roll.ToString("N03")
+
+    End Sub
+
+    Private Async Function ListenUDP(port As Integer, cancellationToken As CancellationToken) As Task
+        Dim UDPClient = New UdpClient()
+        UDPClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, True)
+
+        log($"Listening to UDP on port: {port}")
+
+        Dim iepRemoteEndPoint As IPEndPoint = New IPEndPoint(IPAddress.Any, port)
+
+        UDPClient.Client.Bind(iepRemoteEndPoint)
+
+        listening = True
+        Try
+            Dim strMessage As String = String.Empty
+            Do
+                Try
+
+                    Dim receiveResults As UdpReceiveResult = Await UDPClient.ReceiveAsync().WithCancellation(cancellationToken)
+
+                    Dim x = BitConverter.ToDouble(receiveResults.Buffer, 0)
+                    Dim y = BitConverter.ToDouble(receiveResults.Buffer, 8)
+                    Dim z = BitConverter.ToDouble(receiveResults.Buffer, 16)
+                    Dim yaw = BitConverter.ToDouble(receiveResults.Buffer, 24)
+                    Dim pitch = BitConverter.ToDouble(receiveResults.Buffer, 32)
+                    Dim roll = BitConverter.ToDouble(receiveResults.Buffer, 36)
+
+                    Me.Invoke(Sub() setValues(x, y, z, yaw, pitch, roll))
+
+                Catch e As OperationCanceledException
+                    listening = False
+                End Try
+
+
+            Loop While (listening)
+
+            log($"Closing UDP connection")
+
+            UDPClient.Close()
+            UDPClient.Dispose()
+
+        Catch e As Exception
+            log(e.ToString())
+        End Try
+
+        listening = False
+    End Function
+
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+        Dim port As Integer
+        Try
+            port = Integer.Parse(PortTB.Text)
+        Catch ex As Exception
+            Return
+        End Try
+
+        StopButton.Enabled = True
+        StartButton.Enabled = False
+        cts = New CancellationTokenSource()
+        ListenUDP(port, cts.Token)
+    End Sub
+
+    Private Async Sub StopButton_Click(sender As Object, e As EventArgs) Handles StopButton.Click
+        listening = False
+        StopButton.Enabled = False
+
+
+        If (cts IsNot Nothing) Then
+            cts.Cancel()
+        End If
+        cts = Nothing
+
+        StartButton.Enabled = True
+    End Sub
+End Class
